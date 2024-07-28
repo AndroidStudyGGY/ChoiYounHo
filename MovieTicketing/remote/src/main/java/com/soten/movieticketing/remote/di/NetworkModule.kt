dc7b10d032d1ff92083fb669f95c7d43cb19f605
Choiyounho <maxosa78@gmail.com>
2024-07-28 16:53:21 +0900

    feat: NetworkModule

---
+++ MovieTicketing/remote/src/main/java/com/soten/movieticketing/remote/di/NetworkModule.kt
@@ -0,0 +1,66 @@
+package com.soten.movieticketing.remote.di
+
+import android.util.Log
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.soten.movieticketing.remote.api.ApiInfo
+import com.soten.movieticketing.remote.api.MovieApi
+import com.soten.movieticketing.remote.interceptor.ApiInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val CONTENT_TYPE = "application/json"
+    private const val LOG_TAG = "MovieTicketing"
+
+    private val networkJson: Json = Json {
+        encodeDefaults = true
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(ApiInfo.BASE_URL)
+            .addConverterFactory(networkJson.asConverterFactory(CONTENT_TYPE.toMediaType()))
+            .client(
+                provideOkHttpClient(
+                    ApiInterceptor(),
+                    HttpLoggingInterceptor { log ->
+                        Log.d(LOG_TAG, log)
+                    }.apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    }
+                )
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(): MovieApi {
+        return provideRetrofit().create(MovieApi::class.java)
+    }
+
+    private fun provideOkHttpClient(vararg interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder().run {
+            interceptor.forEach { addInterceptor(it) }
+            build()
+        }
+    }
+}
