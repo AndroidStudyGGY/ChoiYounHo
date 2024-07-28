b0175fdf75f029dc0eca2475387d21c66d727890
Choiyounho <maxosa78@gmail.com>
2024-07-28 16:53:16 +0900

    feat: ApiInterceptor

---
+++ MovieTicketing/remote/src/main/java/com/soten/movieticketing/remote/interceptor/ApiInterceptor.kt
@@ -0,0 +1,19 @@
+package com.soten.movieticketing.remote.interceptor
+
+import com.soten.movieticketing.remote.api.ApiInfo
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+class ApiInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+        val newRequest = request().newBuilder()
+            .addHeader(APP_KEY, ApiInfo.KEY)
+            .build()
+        proceed(newRequest)
+    }
+
+    companion object {
+        private const val APP_KEY = "appKey"
+    }
+}
