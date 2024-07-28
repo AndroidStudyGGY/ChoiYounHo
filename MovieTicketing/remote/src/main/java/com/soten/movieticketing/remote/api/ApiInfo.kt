df50b7b5a7709412ad72713ff549dc5b10683e20
Choiyounho <maxosa78@gmail.com>
2024-07-28 16:53:32 +0900

    feat: ApiInfo

---
+++ MovieTicketing/remote/src/main/java/com/soten/movieticketing/remote/api/ApiInfo.kt
@@ -0,0 +1,10 @@
+package com.soten.movieticketing.remote.api
+
+object ApiInfo {
+    const val BASE_URL = "http://www.kobis.or.kr/kobisopenapi/"
+    const val KEY = "7ec1bb57c28eb56fb8bd2c64aac6bb2e"
+
+    object EndPoints {
+        const val SEARCH = "webservice/rest/movie/searchMovieList.json"
+    }
+}
